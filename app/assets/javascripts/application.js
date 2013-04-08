8221c6901b9b66a4b297fdb4525b765cfe93d42b
Jesse Buchanan <jbinto@jbinto.ca>
2013-04-08 09:29:41 -0400

    Refactor page events in preparation for key handling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -78,8 +78,8 @@ var controlbot = function() {
 
 }();
 
-$(function() {
 
+$(function() {
   /* initialize DOM selectors */
   $_btnForward = $('.btn.forward');
   $_btnReverse = $('.btn.reverse');
@@ -93,65 +93,74 @@ $(function() {
   $_statsStrafe = $('.stats.strafe');
   $_statsTurn = $('.stats.turn');
 
-  /* bind to DOM events */
-
-  /* XXX: How can I trigger these by click and keyboard in a clean way? */
-  $_btnForward.click(function() {
-    controlbot.forward(1);    
-    toggleButtonPair($(this), $_btnReverse); 
-  });
-
-  $_btnReverse.click(function() {
-    controlbot.forward(-1);
-    toggleButtonPair($(this), $_btnForward); 
-  });
-
-  $_btnLeft.click(function() {
-    controlbot.strafe(1);
-    toggleButtonPair($(this), $_btnRight); 
-  });
-
-  $_btnRight.click(function() {
-    controlbot.strafe(-1);
-    toggleButtonPair($(this), $_btnLeft); 
-  });
-
-  $_btnTurnLeft.click(function() {
-    controlbot.turn(1);
-    toggleButtonPair($(this), $_btnTurnRight); 
-  });
-
-  $_btnTurnRight.click(function() {
-    controlbot.turn(-1);
-    toggleButtonPair($(this), $_btnTurnLeft);
-  });
-
-  $_btnStop.click(function() {
-    controlbot.stop();
-    $('.btn').removeClass('active');
-    $(this).toggleClass('active');
-  });
-
+  /* UI helper functions */
   var updateStatsDisplay = function(stats) {
     $_statsForward.text(stats.forward);
     $_statsTurn.text(stats.turn);
     $_statsStrafe.text(stats.strafe);
   }
 
-  var toggleButtonPair = function($active, $inactive) {
+  var toggleButtonPair = function($active, $inactive) {    
     $active.toggleClass('active');
     $inactive.removeClass('active');
+    $_btnStop.removeClass('active');
   }
 
-  /* set up status update */
+  /* Logical event handlers */
+  var forward = function() {
+    controlbot.forward(1);    
+    toggleButtonPair($_btnForward, $_btnReverse);
+  };
 
+  var reverse = function() {
+    controlbot.forward(-1);
+    toggleButtonPair($_btnReverse, $_btnForward); 
+  };
+
+  var left = function() {
+    controlbot.strafe(1);
+    toggleButtonPair($_btnLeft, $_btnRight); 
+  };
+
+  var right = function() {
+    controlbot.strafe(-1);
+    toggleButtonPair($_btnRight, $_btnLeft); 
+  };
+
+  var turnLeft = function() {
+    controlbot.turn(1);
+    toggleButtonPair($_btnTurnLeft, $_btnTurnRight); 
+  };
+
+  var turnRight = function() {
+    controlbot.turn(-1);
+    toggleButtonPair($_btnTurnRight, $_btnTurnLeft);
+  };
+
+  var stop = function() {
+    controlbot.stop();
+    $('.btn').removeClass('active');
+    $_btnStop.toggleClass('active');
+  };
+
+  /* bind to DOM events */
+  $_btnForward.click(function() { forward(); });
+  $_btnReverse.click(function() { reverse(); });
+  $_btnLeft.click(function() { left(); });
+  $_btnRight.click(function() { right(); });
+  $_btnTurnLeft.click(function() { turnLeft(); });
+  $_btnTurnRight.click(function() { turnRight(); });
+  $_btnStop.click(function() { stop(); });
+
+
+  /* set up status update */
   /* This is hacky: how can I do this without polling? */
   setInterval(function() {
     controlbot.getStats(updateStatsDisplay);
   }, 500);
 
-  /* stop the robot! */
-
+  /* stop the robot at the beginning! */
+  stop();
 
 });
 
